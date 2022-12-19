2ad993823bd79b9c5fd3c2de80ca5103b4421fac
刘涛 <10666779@qq.com>
2022-12-19 16:43:23 +0800

    add click event for avatar image

--- app/views/chats/contacts/AvatarImage.js
+++ app/views/chats/contacts/AvatarImage.js
@@ -2,6 +2,6 @@
 import React from "react";
 const LENGTH_NAME = -2;
 
-export default  ({ name }) => {
-	return (<div className="user-avatar">{name && name.slice(LENGTH_NAME)}</div>);
+export default  ({ name, onClick = () => {} }) => {
+	return (<div className="user-avatar" onClick={ onClick }>{name && name.slice(LENGTH_NAME)}</div>);
 };
